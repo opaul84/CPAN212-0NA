01188cd5690e992092c814690ca169c38d21985d
Oshaine <oshaine@omail.com>
2025-01-21 09:55:00 -0500

    week02

---
+++ week02/index.js
@@ -0,0 +1,15 @@
+//const http=require("http")
+import http from "http"
+const app = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to the server')}
+
+        else if (req.url === '/about'){res.end('welcome to about us');
+    }
+})
+
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
+
